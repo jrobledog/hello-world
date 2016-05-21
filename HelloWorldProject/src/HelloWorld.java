bcf0ea30324201cc688b6fc903844e92785dccb3
jrobledog <jesusrobledog@gmail.com>
2016-05-21 18:15:57 +0200

    Update HelloWorld.java
    Commit 4

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Git. This is a commit.");
+		System.out.println("Hello World Git. This is a commit 2.");
 	}
 
 }
